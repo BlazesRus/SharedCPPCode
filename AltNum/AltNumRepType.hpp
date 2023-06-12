b404b980c1c94ed372f6612ce3e3cc6ffb355913
BlazesRus (James) <BlazesRus@yahoo.com>
2023-06-12 00:34:24 -0400

    Updated RepType reference file

--- AltNum/AltNumRepType.hpp
+++ AltNum/AltNumRepType.hpp
@@ -1,68 +1,74 @@
         enum class RepType: int
         {
             NormalType = 0,
+#if !defined(AltNum_DisableTrailingDigits)//For MixedDec format
+            ExtendedNormalType,//Normal Representation with extra trailing digits
+#endif
             NumByDiv,
-//#if defined(AltNum_EnablePIRep)
+#if defined(AltNum_EnablePIRep)
             PINum,
-//#if defined(AltNum_EnablePIPowers)
+#if defined(AltNum_EnablePIPowers)
             PIPower,
-//#endif
-//#if defined(AltNum_EnableAlternativeRepFractionals)
-//#if defined(AltNum_EnableDecimaledPiFractionals)
+#endif
+#if defined(AltNum_EnableAlternativeRepFractionals)
+#if defined(AltNum_EnableDecimaledPiFractionals)
             PiNumByDiv,//  (Value/(ExtraRep*-1))*Pi Representation
-//#endif
+#endif
             PiFractional,//  IntValue/DecimalHalf*Pi Representation
-//#endif
-//#endif
-//#if defined(AltNum_EnableENum)
+#endif
+#endif
+#if defined(AltNum_EnableENum)
             ENum,
-//#if defined(AltNum_EnableAlternativeRepFractionals)
-//#if defined(AltNum_EnableDecimaledEFractionals)
+#if defined(AltNum_EnableAlternativeRepFractionals)
+#if defined(AltNum_EnableDecimaledEFractionals)
             ENumByDiv,//(Value/(ExtraRep*-1))*e Representation
-//#endif
+#endif
             EFractional,//  IntValue/DecimalHalf*e Representation
-//#endif
-//#endif
-//#if defined(AltNum_EnableImaginaryNum)
+#endif
+#endif
+#if defined(AltNum_EnableImaginaryNum)
             INum,
-//#if defined(AltNum_EnableAlternativeRepFractionals)
-//#if defined(AltNum_EnableDecimaledIFractionals)
+#if defined(AltNum_EnableAlternativeRepFractionals)
+#if defined(AltNum_EnableDecimaledIFractionals)
             INumByDiv,//(Value/(ExtraRep*-1))*i Representation
-//#endif
+#endif
             IFractional,//  IntValue/DecimalHalf*i Representation
-//#endif
-//#ifdef AltNum_EnableComplexNumbers
+#endif
+#ifdef AltNum_EnableComplexNumbers
             ComplexIRep,
-//#endif
-//#endif
-//#if defined(AltNum_EnableMixedFractional)
+#endif
+#endif
+#if defined(AltNum_EnableMixedFractional)
             ComplexIRep,
             MixedFrac,//IntValue +- (DecimalHalf*-1)/ExtraRep
             MixedE,
             MixedI,
-//#endif
-//#if defined(AltNum_EnableInfinityRep)
-            ApproachingBottom,//(Approaching Towards Zero is equal to 0.000...1)
-            ApproachingTop,//(Approaching Away from Zero is equal to 0.9999...)
-//#if defined(AltNum_EnableApproachingDivided)
-            ApproachingTopByDiv,//(Approaching Away from Zero is equal to IntValue + 0.9999.../ExtraRep if positive, IntValue - 0.9999.../ExtraRep if negative) 
-//#endif
-//#endif
+#endif
+#if defined(AltNum_EnableInfinityRep)
+            ApproachingBottom,//(Approaching Towards Zero);(IntValue of 0 results in 0.00...1)
+            ApproachingTop,//(Approaching Away from Zero);(IntValue of 0 results in 0.99...9)
+#if defined(AltNum_EnableApproachingDivided)
+            ApproachingMidRight,//(Approaching Away from Zero is equal to IntValue + 1/ExtraRep-ApproachingLeftRealValue if positive, IntValue - 1/ExtraRep+ApproachingLeftRealValue if negative)
+			ApproachingMidLeft,//(Approaching Away from Zero is equal to IntValue + 1/ExtraRep+ApproachingLeftRealValue if positive, IntValue - 1/ExtraRep-ApproachingLeftRealValue if negative) 
+#endif
+#endif
             NaN,
+#if defined(AltNum_EnableNegativeZero)
             NegativeZero,
-//#if defined(AltNum_EnableNearPI)
+#endif
+#if defined(AltNum_EnableNearPI)
             NearPI,//(Approaching Away from Zero is equal to 0.9999...PI)
-//#endif
-//#if defined(AltNum_EnableNearE)
+#endif
+#if defined(AltNum_EnableNearE)
             NearE,//(Approaching Away from Zero is equal to 0.9999...e)
-//#endif
-//#if defined(AltNum_EnableNearI)
+#endif
+#if defined(AltNum_EnableNearI)
             NearI,//(Approaching Away from Zero is equal to 0.9999...i)
-//#endif
-//#if defined(AltNum_EnableUndefinedButInRange)//Such as result of Cos of infinity
+#endif
+#if defined(AltNum_EnableUndefinedButInRange)//Such as result of Cos of infinity
             UndefinedButInRange,
-//#endif
-//#ifndef MixedDec_DisableIntNumByDivisor//Not needed by AltDec since using NumByDivisor with ExtraRep value instead
+#endif
+#ifndef MixedDec_DisableIntNumByDivisor//Not needed by AltDec since using NumByDivisor with ExtraRep value instead
 //			//IntValue/(DecimalHalf*-1)
 //			//For DecimalHalf values of between 0 and ENumBreakpoint if MediumDecV2_EnableERep toggled
 //			//For DecimalHalf Values:
@@ -70,6 +76,6 @@
 //			//between 0 and MidFromBottomRep if MediumDecV2_DisableInfinityRep not toggled and MediumDecV2_EnableApproachingMidDec is toggled
 //			//between 0 and -2147483648 if no infinity representations are enabled
             IntNumByDivisor,
-//#endif
+#endif
             UnknownType
         };
