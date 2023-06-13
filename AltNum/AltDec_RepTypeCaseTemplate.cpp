aeb960314f4967e5fd3981fe3eb111845e572a6e
BlazesRus (James) <BlazesRus@yahoo.com>
2023-06-13 18:01:56 -0400

    Adding code scrap to use as template to make sure has operations for each RepType enabled (for replacing code later)

---
+++ AltNum/AltDec_RepTypeCaseTemplate.cpp
@@ -0,0 +1,95 @@
+                    case RepType::NormalType:
+						break;
+//#if defined(AltNum_EnablePIRep)
+//                    case RepType::PINum:
+//					    break;
+//#endif
+//#if defined(AltNum_EnableENum)
+//                    case RepType::ENum:
+//#endif
+//#if defined(AltNum_EnableImaginaryNum)
+//                    case RepType::INum:
+//                        break;
+//#endif
+//
+//#if AltNum_EnableMixedFractional
+//                    case RepType::ComplexIRep:
+//                        break;
+//                    case RepType::MixedFrac://IntValue +- (DecimalHalf*-1)/ExtraRep
+//						break;
+//#if defined(AltNum_EnableENum)
+//                    case RepType::MixedE:
+//						break;
+//#endif
+//#if defined(AltNum_EnableImaginaryNum)
+//                    case MixedI:
+//						break;
+//#endif
+//#endif
+//
+//#if defined(AltNum_EnableApproachingDivided)
+//                    case RepType::ApproachingBottom:
+//                        break;
+//                    case RepType::ApproachingTop:
+//                        break;
+//#if defined(AltNum_EnableApproachingDivided)
+//                    case RepType::ApproachingBottom:
+//                        break;
+//                    case RepType::ApproachingTop:
+//                        break;
+//#endif
+//#endif
+//
+//#if defined(AltNum_EnableAlternativeRepFractionals)
+//                    case RepType::NumByDiv:
+//                        break;
+//#if defined(AltNum_EnablePIRep)
+//                    case RepType::PiFractional://  IntValue/DecimalHalf*Pi Representation
+//                        break;
+//#endif
+//#if defined(AltNum_EnableENum)
+//                    case RepType::EFractional://  IntValue/DecimalHalf*e Representation
+//                        break;
+//#endif
+//#if defined(AltNum_EnableImaginaryNum)
+//                    case RepType::IFractional://  IntValue/DecimalHalf*i Representation
+//                        break;
+//#endif
+//#if defined(AltNum_EnableDecimaledPiFractionals)
+//                    case RepType::PiNumByDiv://  (Value/(ExtraRep*-1))*Pi Representation
+//                        break;
+//#endif
+//#if defined(AltNum_EnableDecimaledEFractionals)
+//                    case RepType::ENumByDiv://(Value/(ExtraRep*-1))*e Representation
+//                        break;
+//#endif
+//#if defined(AltNum_EnableImaginaryNum)
+//                    case RepType::INumByDiv://(Value/(ExtraRep*-1))*i Representation
+//                        break;
+//#endif
+//#endif
+//#if defined(AltNum_EnablePIPowers)
+//                    case RepType::PIPower:
+//						break;
+//#endif
+//#if defined(AltNum_EnableComplexNumbers)
+//                    case RepType::ComplexIRep:
+//						break;
+//#endif
+//#if defined(AltNum_EnableUndefinedButInRange)//Such as result of Cos of infinity
+//                    case RepType::UndefinedButInRange:
+//						break;
+//#endif
+                    case RepType::NaN:
+						throw "Can't perform operations with NaN";
+						break;
+#if defined(AltNum_EnableNegativeZero)
+                    case RepType::NegativeZero://Treat operation as with Zero in most cases
+						break;
+#endif
+                    default:
+						throw static_cast<RepType>(LRep)+" RepType addition not supported yet";
+						//throw static_cast<RepType>(LRep)+" RepType addition with"+static_cast<RepType>(RRep)+"not supported yet";
+                        //self.CatchAllAddition(Value, LRep, RRep);
+                        break;
+                }
