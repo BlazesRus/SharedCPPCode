aa2dc9128bde752cd21b213dfe7d45c3fc3b3da3
BlazesRus (James) <BlazesRus@yahoo.com>
2023-06-14 18:28:22 -0400

    AddOp code updated with the new representation types (for the Same RepType to same operations)

--- AltNum/AltDec.hpp
+++ AltNum/AltDec.hpp
@@ -3207,17 +3207,31 @@ public:
                 switch (LRep)
                 {
                     case RepType::NormalType:
+#if defined(AltNum_EnablePIRep)
                     case RepType::PINum:
+#endif
 #if defined(AltNum_EnableENum)
                     case RepType::ENum:
-#elif defined(AltNum_EnableImaginaryNum)
+#endif
+#if defined(AltNum_EnableImaginaryNum)
                     case RepType::INum:
 #endif
-                        self.BasicAddOp(Value);
-                        break;
-
-#if AltNum_EnableMixedFractional
+#if defined(AltNum_EnableMixedFractional)
+                    case RepType::MixedFrac://IntValue +- (DecimalHalf*-1)/ExtraRep
+#if defined(AltNum_EnablePINum)
+                    case RepType::MixedPi:
 #endif
+#if defined(AltNum_EnableENum)
+                    case RepType::MixedE:
+#endif
+#if defined(AltNum_EnableImaginaryNum)
+                    case MixedI:
+#endif
+#endif
+						self.BasicAddOp(Value);
+						break;
+
+#if defined(AltNum_EnableApproachingDivided)
                     case RepType::ApproachingBottom:
                         if (self.IntValue == NegativeRep)
                         {
@@ -3312,29 +3326,84 @@ public:
                             }
                         }
                         break;
+
+//#if defined(AltNum_EnableApproachingDivided)
+//                    case RepType::ApproachingBottom:
+//                        break;
+//                    case RepType::ApproachingTop:
+//                        break;
+//#endif
+#endif
+
+#if defined(AltNum_EnableAlternativeRepFractionals)
                     case RepType::NumByDiv:
-                        if(self.ExtraRep==Value.ExtraRep)
-                            self.BasicAddOp(Value);
-                        else
-                            self.CatchAllAddition(Value, LRep, RRep);
-                        break;
+#if defined(AltNum_EnablePIRep)
+                    case RepType::PiFractional://  IntValue/DecimalHalf*Pi Representation
+#endif
 #if defined(AltNum_EnableENum)
-                    case RepType::ENumByDiv:
+                    case RepType::EFractional://  IntValue/DecimalHalf*e Representation
+#endif
+
+#if defined(AltNum_EnableDecimaledPiFractionals)
+                    case RepType::PiNumByDiv://  (Value/(ExtraRep*-1))*Pi Representation
+#endif
+#if defined(AltNum_EnableDecimaledEFractionals)
+                    case RepType::ENumByDiv://(Value/(ExtraRep*-1))*e Representation
+#endif
+#if defined(AltNum_EnableMixedFractional)
+                    case RepType::MixedFracByDiv://IntValue +- (DecimalHalf*-1)/ExtraRep
+#if defined(AltNum_EnablePINum)
+                    case RepType::MixedPiByDiv:
+#endif
+#if defined(AltNum_EnableENum)
+                    case RepType::MixedEByDiv:
+#endif
+#endif
                         if (self.ExtraRep == Value.ExtraRep)
                             self.BasicAddOp(Value);
-                        else
-                            self.CatchAllAddition(Value, LRep, RRep);
-                        break;
-#elif defined(AltNum_EnableImaginaryNum)
-                    case RepType::INumByDiv:
-                        if (self.ExtraRep == Value.ExtraRep)
-                            self.BasicAddOp(Value);
-                        else
-                            self.CatchAllAddition(Value, LRep, RRep);
+						else
+						{
+							self.ConvertToNormalRep(LRep); value.ConvertToNormalRep(LRep);
+							self.BasicAddOp(Value);
+						}
                         break;
 #endif
+#if defined(AltNum_EnableAlternativeRepFractionals) && defined(AltNum_EnableImaginaryNum)
+                    case RepType::IFractional://  IntValue/DecimalHalf*i Representation
+                    case RepType::INumByDiv://(Value/(ExtraRep*-1))*i Representation
+                    case MixedI:
+                        if (self.ExtraRep == Value.ExtraRep)
+                            self.BasicAddOp(Value);
+						else
+						{
+							self.ConvertToNormalIRep(LRep); value.ConvertToNormalIRep(LRep);
+							self.BasicAddOp(Value);
+						}
+#endif
+
+//#if defined(AltNum_EnablePIPowers)
+//                    case RepType::PIPower:
+//						break;
+//#endif
+//#if defined(AltNum_EnableComplexNumbers)
+//                    case RepType::ComplexIRep:
+//						break;
+//#endif
+//#if defined(AltNum_EnableUndefinedButInRange)//Such as result of Cos of infinity
+//                    case RepType::UndefinedButInRange:
+//						break;
+//#endif
+                    case RepType::Undefined:
+                    case RepType::NaN:
+						throw "Can't perform operations with NaN or Undefined number";
+						break;
+#if defined(AltNum_EnableNegativeZero)
+                    case RepType::NegativeZero://Treat operation as with Zero in most cases(only used in very rare cases)
+						break;
+#endif
                     default:
-                        self.CatchAllAddition(Value, LRep, RRep);
+						throw static_cast<RepType>(LRep)+" RepType addition not supported yet";
+						//throw static_cast<RepType>(LRep)+" RepType addition with"+static_cast<RepType>(RRep)+"not supported yet";
                         break;
                 }
             }
