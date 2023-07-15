9cb0bf4c42efaa92932d962c6de1d44801e37be8
BlazesRus (James) <BlazesRus@yahoo.com>
2023-07-14 21:41:13 -0400

    separating some functions into methods in separate files

---
+++ AltNum/AltDec_RepToRepOperations.cpp
@@ -0,0 +1,17 @@
+#include "AltDec.hpp"
+
+bool MediumDecVariant::RepToRepAddOp(RepType& LRep, RepType& RRep, MediumDecVariant& self, MediumDecVariant& Value)
+{
+}
+
+bool MediumDecVariant::RepToRepSubOp(RepType& LRep, RepType& RRep, MediumDecVariant& self, MediumDecVariant& Value)
+{
+}
+
+bool MediumDecVariant::RepToRepMultOp(RepType& LRep, RepType& RRep, MediumDecVariant& self, MediumDecVariant& Value)
+{
+}
+
+bool MediumDecVariant::RepToRepDivOp(RepType& LRep, RepType& RRep, MediumDecVariant& self, MediumDecVariant& Value)
+{
+}
