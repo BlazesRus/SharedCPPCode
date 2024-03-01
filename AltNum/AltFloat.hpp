da3f5b5d115813d8c6356677e306b6c459145a95
BlazesRus (James) <BlazesRus@yahoo.com>
2024-03-01 18:04:25 -0500

    Adding almost black AltFloat file (as possible replacement for float later for trailing digits)

---
+++ AltNum/AltFloat.hpp
@@ -0,0 +1,13 @@
+// ***********************************************************************
+// Code Created by James Michael Armstrong (https://github.com/BlazesRus)
+// Latest Code Release at https://github.com/BlazesRus/BlazesRusSharedCode
+// ***********************************************************************
+#pragma once
+
+namespace BlazesRusCode
+{
+	//Designed to store trailing digits of number to reduce trunction loss(as replacement for float)
+    class DLL_API AltFloat
+    {
+	}
+}
