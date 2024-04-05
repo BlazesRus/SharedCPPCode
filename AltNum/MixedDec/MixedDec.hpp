9ef3561ce958db0332d41f8c6c1fd7900b344f6c
BlazesRus (James) <BlazesRus@yahoo.com>
2024-04-05 11:20:59 -0400

    empty commit

--- AltNum/MixedDec/MixedDec.hpp
+++ AltNum/MixedDec/MixedDec.hpp
@@ -1,4 +1,4 @@
-// ***********************************************************************
+﻿// ***********************************************************************
 // Code Created by James Michael Armstrong (https://github.com/BlazesRus)
 // Latest Code Release at https://github.com/BlazesRus/BlazesRusSharedCode
 // ***********************************************************************
