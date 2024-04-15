0e47ee5b1d957539ae509435768f21ce1faf404a
BlazesRus (James) <BlazesRus@yahoo.com>
2024-04-15 00:24:06 -0400

    Moved some preprocessors to base preprocessor file

--- AltNum/MediumDec/MediumDecPreprocessors.h
+++ AltNum/MediumDec/MediumDecPreprocessors.h
@@ -12,6 +12,18 @@ AltNum_UseIntForDecimalHalf = Use signed int instead of using a custom bitfield 
 	(uses old behavior); Disabled for now(forcing usage of bitfield instead)
 AltNum_EnableMirroredSection = Enable new Bitfield structure usage instead of using signed int (Not fully implimented yet)
 */
+
+#if defined(AltNum_EnableAutoToggleOfPreferedSettings)
+    #define AltNum_EnablePiRep
+    #define AltNum_EnableERep
+    //#define AltNum_EnableInfinityRep
+	//#define AltNum_EnableFractionals
+    //#define AltNum_EnableDecimaledPiFractionals
+    #define AltNum_EnableApproachingValues
+    //#define AltNum_UseDeveloperExtraDefaults//Turns on extra defaults just for testing
+	#define AltNum_EnableMirroredSection
+#endif
+
 #if defined(AltNum_EnableMirroredSection)
 	#include "..\MirroredInt.hpp"
 	#define IntHalfType MirroredInt
@@ -30,4 +42,136 @@ AltNum_EnableMirroredSection = Enable new Bitfield structure usage instead of us
 	#define RepTypeUnderlayer unsigned short
 #else
 	#define RepTypeUnderlayer unsigned int
+#endif
+
+#if defined(AltNum_EnablePiRep) && defined(AltNum_DisablePiRep)
+    #undef AltNum_DisablePiRep
+#endif
+
+#if defined(AltNum_EnableImaginaryInfinity)
+    #define AltNum_EnableImaginaryNum
+	#define AltNum_EnableInfinityRep
+#endif
+
+#if define(AltNum_EnableIRep)
+	#define AltNum_EnableImaginaryNum
+#endif
+
+//Force required flags to be enabled if AltNum_EnableApproachingDivided toggled
+#if !defined(AltNum_EnableApproachingValues)
+	#if defined(AltNum_EnableApproachingDivided)
+		#define AltNum_EnableApproachingValues
+	#elif defined(AltNum_EnableApproachingPi) || defined(AltNum_EnableApproachingE) || defined(AltNum_EnableApproachingI)
+		#define AltNum_EnableApproachingValues
+	#endif
+#endif
+
+#if defined(AltNum_UseIntForDecimalHalf)
+
+	//Disable Mixed Fractionals for now if PartialInt is not being used
+	#if defined(AltNum_EnableMixedFractional)
+		#undef AltNum_EnableMixedFractional
+	#endif
+
+/*	#if defined(AltNum_EnableMixedPiFractional) || defined(AltNum_EnableMixedEFractional) || defined(AltNum_EnableMixedIFractional)
+		#define AltNum_EnableAlternativeMixedFrac
+		#if !defined(AltNum_EnableMixedFractional)
+			#define AltNum_EnableMixedFractional
+		#endif
+		#if !defined(AltNum_EnableAlternativeRepFractionals)
+			#define AltNum_EnableAlternativeRepFractionals
+		#endif
+		#if !defined(AltNum_EnablePiRep) && defined(AltNum_EnableMixedPiFractional)
+			#define AltNum_EnablePiRep
+		#endif
+		#if !defined(AltNum_EnableERep) && defined(AltNum_EnableMixedEFractional)
+			#define AltNum_EnableERep
+		#endif
+		#if !defined(AltNum_EnableImaginaryNum) && defined(AltNum_EnableMixedIFractional)
+			#define AltNum_EnableImaginaryNum
+		#endif
+	#endif*/
+
+/*	#if defined(AltNum_EnableMixedPiFractional) && defined(AltNum_EnableDecimaledPiFractionals)
+		#define AltNum_MixedPiHasDecimaledFracAccess
+	#elif defined(AltNum_EnableMixedEFractional) && defined(AltNum_EnableDecimaledEFractionals)
+		#define AltNum_MixedEHasDecimaledFracAccess
+	#elif defined(AltNum_EnableMixedIFractional) && defined(AltNum_EnableDecimaledIFractionals)
+		#define AltNum_MixedIHasDecimaledFracAccess
+	#endif*/
+
+/*	#if defined(AltNum_EnableMixedPiFractional) || defined(AltNum_EnableMixedEFractional) || defined(AltNum_EnableMixedEFractional)
+		#define AltNum_MixedAltEnabled//Either MixedPi, MixedE, or MixedI enabled
+	#endif*/
+
+	#if defined(AltNum_EnablePiRep)
+		#if defined(AltNum_EnableFractionals)
+			#define AltNum_EnablePiFractional
+		#endif
+	#endif
+	
+	#if defined(AltNum_EnableERep)
+		#if defined(AltNum_EnableFractionals)
+			#define AltNum_EnableEFractional
+		#endif
+	#endif
+	
+	#if defined(AltNum_EnableImaginaryNum)
+		#if defined(AltNum_EnableFractionals)
+			#define AltNum_EnableIFractional
+		#endif
+	#endif
+
+#else
+
+	#if defined(AltNum_EnablePiRep)
+		#if defined(AltNum_EnableFractionals)
+			#define AltNum_EnableDecimaledPiFractionals
+		#endif
+	#endif
+	
+	#if defined(AltNum_EnableERep)
+		#if defined(AltNum_EnableFractionals)
+			#define AltNum_EnableDecimaledEFractionals
+		#endif
+	#endif
+	
+	#if defined(AltNum_EnableImaginaryNum)
+		#if defined(AltNum_EnableFractionals)
+			#define AltNum_EnableDecimaledIFractionals
+		#endif
+	#endif
+
+#endif
+
+#if defined(AltNum_AvoidUsingLargeInt)
+    #undef AltNum_UseOldDivisionCode
+#endif
+
+#if defined(AltNum_EnableApproachingPi) && !defined(AltNum_EnablePiRep)
+    #define AltNum_EnablePiRep
+#endif
+
+#if defined(AltNum_EnableApproachingE) && !defined(AltNum_EnableERep)
+	#define AltNum_EnableERep
+#endif
+
+#if defined(AltNum_EnableApproachingI) && !defined(AltNum_EnableImaginaryNum)
+    #define AltNum_EnableImaginaryNum
+#endif
+
+#if defined(AltNum_EnableMixedPiFractional) || defined(AltNum_EnableMixedEFractional)
+    #define AltNum_MixedPiOrEEnabled//Either MixedPi or MixedE enabled
+#endif
+
+#if defined(AltNum_MixedPiHasDecimaledFracAccess) || defined(AltNum_MixedEHasDecimaledFracAccess) || defined(AltNum_MixedPiHasDecimaledFracAccess)
+    #define AltNum_MixedAltFracHasDecimaledFractionalAccess
+#endif
+
+#if defined(AltNum_MixedPiHasDecimaledFracAccess)|| defined(AltNum_MixedEHasDecimaledFracAccess)
+    #define AltNum_MixedPiOrEHasDecimaledFracAccess
+#endif
+
+#if defined(AltNum_EnableDecimaledPiFractionals) || defined(AltNum_EnableDecimaledEFractionals)
+    #define AltNum_EnableDecimaledPiOrEFractionals
 #endif
