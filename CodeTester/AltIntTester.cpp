b6cbf72fb82b3e4b435e6cf2b4e77e909ab5603a
BlazesRus (James) <BlazesRus@yahoo.com>
2024-06-23 22:57:25 -0400

    MirroredInt giving 4 unresolved external symbol errors

--- CodeTester/AltIntTester.cpp
+++ CodeTester/AltIntTester.cpp
@@ -8,12 +8,10 @@
 #include "..\AltNum\AlternativeInt\PartialInt.hpp"
 #include "..\AltNum\AlternativeInt\MirroredInt.hpp"
 #include "..\AltNum\AlternativeInt\FlaggedInt.hpp"
-#include "..\AltNum\MediumDec\ReducedMediumDec.hpp"
 
 using MirroredInt = BlazesRusCode::MirroredInt;
 using PartialInt = BlazesRusCode::PartialInt;
 using FlaggedInt = BlazesRusCode::FlaggedInt;
-using MediumDec = BlazesRusCode::MediumDec;
 #include <Windows.h>//Windows.h detects some errors?
 
 int main()
@@ -21,12 +19,14 @@ int main()
     std::ostringstream streamObj;
     streamObj << std::fixed << std::setprecision(99);
     MirroredInt test01 = 5;
-    PartialInt test02 = 4;
-    FlaggedInt test03 = 3;
+    MirroredInt test02 = 4;
+    test02 = test01;
+    MirroredInt test03 = test01;
+    PartialInt Partialtest02 = 4;
+    FlaggedInt Flaggedtest03 = 3;
     test01 *= 10;
     ++test02;
     test03 *= ++test03;
-    MediumDec test04 = 5;
     //Need to fix debug output
     //Outputting to debug output based on https://www.codeproject.com/Articles/1053/Using-an-Output-Stream-for-Debugging
 
