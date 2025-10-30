b3cdc14dab0cec9a3c85e6c58977e410efe41cbd
BlazesRus (James) <BlazesRus@yahoo.com>
2025-10-30 04:46:48 -0400

    UnsignedLike concept added

--- AltNum/Concepts/IntegerConcept.hpp
+++ AltNum/Concepts/IntegerConcept.hpp
@@ -5,12 +5,38 @@ namespace BlazesRusCode
     //Integer type (concept)
     template<typename T>
     concept IntegerType = std::is_integral_v<T>;
-		
+    
     //Unsigned Integer type (concept)
     template<typename T>
     concept UnsignedIntegerType = std::is_unsigned_v<T>&&std::is_integral_v<T>;
-		
+    
     //Signed Integer type (concept)
     template<typename T>
     concept SignedIntegerType = std::is_signed_v<T>&&std::is_integral_v<T>;
+    
+    template<class T>
+    concept UnsignedLike = requires(T a) {
+      // construction of canonical values
+      { T{0} } -> std::same_as<T>;
+      { T{1} } -> std::same_as<T>;
+
+      // bitwise ops on same width
+      { a & a } -> std::same_as<T>;
+      { a | a } -> std::same_as<T>;
+      { a ^ a } -> std::same_as<T>;
+      { ~a }    -> std::same_as<T>;
+
+      // comparisons and zero checks
+      { a == T{0} } -> std::convertible_to<bool>;
+      { a != T{0} } -> std::convertible_to<bool>;
+      { a <  a }    -> std::convertible_to<bool>;
+      { a <= a }    -> std::convertible_to<bool>;
+      { a >  a }    -> std::convertible_to<bool>;
+      { a >= a }    -> std::convertible_to<bool>;
+
+      // shifts with the same type as index
+      { a << T{1} } -> std::same_as<T>;
+      { a >> T{1} } -> std::same_as<T>;
+      { T{1} << T{1} } -> std::same_as<T>;
+    };
 }
